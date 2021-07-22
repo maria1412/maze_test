721ff34a5b9e16e1ed6d351882f9bfc98f51fa88
maria1412 <82562294+maria1412@users.noreply.github.com>
2021-07-22 15:45:22 +0900

    更新 main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,186 @@ namespace SpriteKind {
     export const key = SpriteKind.create()
     export const door = SpriteKind.create()
 }
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mainchara,
+    [img`
+        . . . f f f f f . . . . . 
+        . f f f f f f f f f . . . 
+        . f f f f f f 8 f f f . . 
+        f f f f 8 f f f 8 f f . . 
+        f 8 f f 8 8 f f f 8 8 f f 
+        f 8 8 f f f f e f f f f f 
+        f f f f f f f e e f f f . 
+        f f e e f b 9 e e f f . . 
+        . f e 4 e 1 f 4 4 f . . . 
+        . f f f f f f f f f . . . 
+        . . f f f f 8 8 f f . . . 
+        . . f f f f f f f f . . . 
+        . . e 4 4 e f f f f . . . 
+        . . f e e f f f f f . . . 
+        . . . f f f f f f . . . . 
+        . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . 
+        . . . f f f f f f . . . . 
+        . f f f f f f f f f . . . 
+        . f f f f f f 8 f f f . . 
+        f f f f 8 f f f 8 f f f . 
+        f 8 f f 8 8 f f f 8 8 f f 
+        f 8 8 f f f f e f f f f f 
+        f f f f f f f e e f f f . 
+        f f e e f b 9 e e f f . . 
+        . f e 4 e 1 f 4 4 f f . . 
+        . f f f f f f f f f . . . 
+        . . f f f 4 e 8 f f . . . 
+        . . f f 4 4 e f f f . . . 
+        . f f f e e f f f f f . . 
+        . f f f f f f f f f f . . 
+        . . f f . . . f f f . . . 
+        `,img`
+        . . . . . . . . . . . . . 
+        . . . f f f f f f . . . . 
+        . f f f f f f f f f . . . 
+        . f f f f f f 8 f f f . . 
+        f f f f 8 f f f 8 f f f . 
+        f 8 f f 8 8 f f f 8 8 f f 
+        f 8 8 f f f f e f f f f f 
+        f f f f f f f e e f f f . 
+        f f e e f b 9 e e f f f . 
+        f f e 4 e 1 f 4 4 f f . . 
+        . f f f f f f f f f . . . 
+        . 4 4 f 8 8 8 f f f . . . 
+        . e 4 f f f f f f f . . . 
+        . f e e f f f f f f f . . 
+        . f f f f f f f f f f . . 
+        . . f f . . . f f f . . . 
+        `],
+    100,
+    true
+    )
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mainchara,
+    [img`
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f f f f 8 f f f . . 
+        f f f f f f 8 8 f f f 8 . 
+        f f f 8 f f f f f f f 8 . 
+        8 8 8 f f f f f f f 8 8 . 
+        f f f f f e e f f f f f . 
+        f f f b 9 e e 9 b f f f . 
+        . f 4 1 f 4 4 f 1 4 f . . 
+        . f f f f f f f f f f . . 
+        . f f f f f f f f f f . . 
+        f f 8 8 f f f f 8 8 f f . 
+        e 4 f f f f f f f f 4 e . 
+        e e f f f f f f f f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . 
+        . . . . . f f f f . . . . 
+        . . . f f f f f f f f . . 
+        . . f f f f f f 8 f f f . 
+        f f f f f f f 8 8 f f f 8 
+        f f f f 8 f f f f f f f 8 
+        . 8 8 8 f f f e e f f 8 8 
+        . f f f f f e e f f 8 8 f 
+        . f f f b 9 e e 9 b f f f 
+        . f f 4 1 f 4 4 f 1 4 f f 
+        . . f f f f f f f f f f f 
+        . f e 8 8 f f f 8 4 4 4 f 
+        . e 4 f f f f f f 4 4 e . 
+        . . . f f f f f f e e . . 
+        . . . f f f f f f f . . . 
+        . . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . 
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f 8 f f f f f f . . 
+        8 f f f 8 8 f f f f f f f 
+        8 f f f f f f f 8 f f f f 
+        8 8 f f e e f f f 8 8 8 . 
+        f 8 8 f f e e f f f f f . 
+        f f f b 9 e e 9 b f f f . 
+        f f 4 1 f 4 4 f 1 4 f f . 
+        f f f f f f f f f f f . . 
+        f 4 4 4 8 f f f 8 8 e f . 
+        . e 4 4 f f f f f f 4 e . 
+        . . e e f f f f f f . . . 
+        . . . f f f f f f f . . . 
+        . . . . . . . f f f . . . 
+        `],
+    100,
+    true
+    )
+})
+controller.anyButton.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, mainchara)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mainchara,
+    [img`
+        . . . . . f f f f f . . . 
+        . . . f f f f f f f f f . 
+        . . f f f 8 f f f f f f . 
+        . . f f 8 f f f 8 f f f f 
+        f f 8 8 f f f 8 8 f f 8 f 
+        f f f f f e f f f f 8 8 f 
+        . f f f e e f f f f f f f 
+        . . f f e e 9 b f e e f f 
+        . . . f 4 4 f 1 e 4 e f . 
+        . . . f f f f f f f f f . 
+        . . . f f 8 8 f f f f . . 
+        . . . f f f f f f f f . . 
+        . . . f f f f e 4 4 e . . 
+        . . . f f f f f e e f . . 
+        . . . . f f f f f f . . . 
+        . . . . . . f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . 
+        . . . f f f f f f f f f . 
+        . . f f f 8 f f f f f f . 
+        . f f f 8 f f f 8 f f f f 
+        f f 8 8 f f f 8 8 f f 8 f 
+        f f f f f e f f f f 8 8 f 
+        . f f f e e f f f f f f f 
+        . . f f e e 9 b f e e f f 
+        . . f f 4 4 f 1 e 4 e f . 
+        . . . f f f f f f f f f . 
+        . . . f f 8 e 4 f f f . . 
+        . . . f f f e 4 4 f f . . 
+        . . f f f f f e e f f f . 
+        . . f f f f f f f f f f . 
+        . . . f f f . . . f f . . 
+        `,img`
+        . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . 
+        . . . f f f f f f f f f . 
+        . . f f f 8 f f f f f f . 
+        . f f f 8 f f f 8 f f f f 
+        f f 8 8 f f f 8 8 f f 8 f 
+        f f f f f e f f f f 8 8 f 
+        . f f f e e f f f f f f f 
+        . f f f e e 9 b f e e f f 
+        . . f f 4 4 f 1 e 4 e f f 
+        . . . f f f f f f f f f . 
+        . . . f f f 8 8 8 f 4 4 . 
+        . . . f f f f f f f 4 e . 
+        . . f f f f f f f e e f . 
+        . . f f f f f f f f f f . 
+        . . . f f f . . . f f . . 
+        `],
+    100,
+    true
+    )
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.door, function (sprite, otherSprite) {
     if (getketFlag == 1) {
         door.destroy()
@@ -11,29 +191,237 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.door, function (sprite, otherSpr
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileGrass2, function (sprite, location) {
     game.over(true, effects.confetti)
 })
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mainchara,
+    [img`
+        . . . . f f f f . . . . . 
+        . . f f 8 8 8 8 f f . . . 
+        . f f 8 8 8 8 8 8 f f . . 
+        f f 8 8 8 8 8 8 8 8 f f . 
+        f f 8 8 f 8 8 8 8 8 8 f . 
+        f f f f f 8 8 8 f 8 8 f . 
+        f f f f 8 8 8 f 8 8 f f . 
+        f f f f f f f f f f f f . 
+        f f f f f f f f f f f f . 
+        . f f f f f f f f f f . . 
+        . f f f f f f f f f f . . 
+        f f f f f f f f f f f f . 
+        e 4 f f f f f f f f 4 e . 
+        e e f f f f f f f f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . 
+        . . . . . f f f f . . . . 
+        . . . f f 8 8 8 8 f f . . 
+        . f f f 8 8 8 8 8 8 f f . 
+        f f 8 8 8 8 8 8 8 8 8 f f 
+        f 8 8 8 8 f 8 8 8 8 8 8 f 
+        . f f f f 8 8 8 8 f 8 8 f 
+        . f f f f 8 8 f 8 8 8 f f 
+        . f f f f f f f f f f f f 
+        . f f f f f f f f f f f f 
+        . . f f f f f f f f f f . 
+        . . f f f f f f f f f f . 
+        . . f f f f f f f f f f f 
+        . . 4 f f f f f f f 4 4 e 
+        . . e f f f f f f f e e . 
+        . . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . 
+        . . . . . f f f f . . . . 
+        . . . f f 8 8 8 8 f f . . 
+        . . f f 8 8 8 8 8 8 f f . 
+        . f f f 8 8 8 8 8 8 8 f f 
+        f f f 8 8 8 8 8 8 8 8 8 f 
+        f f 8 8 8 f 8 8 8 8 8 8 f 
+        . f f f f f 8 8 8 f 8 f f 
+        . f f f f 8 8 f f f f f f 
+        . . f f f f f f f f f f f 
+        . . f f f f f f f f f f . 
+        . . f f f f f f f f f f . 
+        . f f f f f f f f f f f . 
+        . e 4 4 f f f f f f f 4 . 
+        . . e e f f f f f f f e . 
+        . . . . . . . . f f f . . 
+        `],
+    100,
+    true
+    )
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.key, function (sprite, otherSprite) {
     music.magicWand.play()
     key.destroy(effects.halo, 200)
     getketFlag = 1
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (true) {
+        shuriken = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . 9 . . . . . . . 
+            . . . . . . . 9 f . . . . . . . 
+            . . . . . 6 f f f 6 . . . . . . 
+            . . . . . . 6 f f f 6 . . . . . 
+            . . . . . . . f 9 . . . . . . . 
+            . . . . . . . 9 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mainchara, controller.dx(), controller.dy())
+        animation.runImageAnimation(
+        shuriken,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . 9 . . . . . . . 
+            . . . . . . . 9 f . . . . . . . 
+            . . . . . 6 f f f 6 . . . . . . 
+            . . . . . . 6 f f f 6 . . . . . 
+            . . . . . . . f 9 . . . . . . . 
+            . . . . . . . 9 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 9 . 6 6 . . . . . . 
+            . . . . . . 9 f f . . . . . . . 
+            . . . . . . . f f 9 . . . . . . 
+            . . . . . . 6 6 . 9 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . 6 . . . . . . . 
+            . . . . . . . 6 f . . . . . . . 
+            . . . . . 9 f f f 9 . . . . . . 
+            . . . . . . 9 f f f 9 . . . . . 
+            . . . . . . . f 6 . . . . . . . 
+            . . . . . . . 6 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 6 . 9 9 . . . . . . 
+            . . . . . . 6 f f . . . . . . . 
+            . . . . . . . f f 6 . . . . . . 
+            . . . . . . 9 9 . 6 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . 9 . . . . . . . 
+            . . . . . . . 9 f . . . . . . . 
+            . . . . . 6 f f f 6 . . . . . . 
+            . . . . . . 6 f f f 6 . . . . . 
+            . . . . . . . f 9 . . . . . . . 
+            . . . . . . . 9 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 9 . 6 6 . . . . . . 
+            . . . . . . 9 f f . . . . . . . 
+            . . . . . . . f f 9 . . . . . . 
+            . . . . . . 6 6 . 9 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . 6 . . . . . . . 
+            . . . . . . . 6 f . . . . . . . 
+            . . . . . 9 f f f 9 . . . . . . 
+            . . . . . . 9 f f f 9 . . . . . 
+            . . . . . . . f 6 . . . . . . . 
+            . . . . . . . 6 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        50,
+        true
+        )
+    }
+})
+let shuriken: Sprite = null
 let door: Sprite = null
 let getketFlag = 0
 let key: Sprite = null
-let mainchara = sprites.create(img`
+let mainchara: Sprite = null
+mainchara = sprites.create(img`
     . . . . f f f f . . . . . 
     . . f f f f f f f f . . . 
-    . f f f f f f c f f f . . 
-    f f f f f f c c f f f c . 
-    f f f c f f f f f f f c . 
-    c c c f f f e e f f c c . 
-    f f f f f e e f f c c f . 
-    f f f b f e e f b f f f . 
+    . f f f f f f 8 f f f . . 
+    f f f f f f 8 8 f f f 8 . 
+    f f f 8 f f f f f f f 8 . 
+    8 8 8 f f f f f f f 8 8 . 
+    f f f f f e e f f 8 8 f . 
+    f f f b 9 e e 9 b f f f . 
     . f 4 1 f 4 4 f 1 4 f . . 
-    . f e 4 4 4 4 4 4 e f . . 
-    . f f f e e e e f f f . . 
-    f e f b 7 7 7 7 b f e f . 
-    e 4 f 7 7 7 7 7 7 f 4 e . 
-    e e f 6 6 6 6 6 6 f e e . 
+    . f f f f f f f f f f . . 
+    . f f f f f f f f f f . . 
+    f f 8 8 f f f f 8 8 f f . 
+    e 4 f f f f f f f f 4 e . 
+    e e f f f f f f f f e e . 
     . . . f f f f f f . . . . 
     . . . f f . . f f . . . . 
     `, SpriteKind.Player)
